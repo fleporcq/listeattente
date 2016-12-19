21b57a35f11218a15377f0a565d72bbfc59fd21c
François LEPORCQ <fleporcq@gmail.com>
2016-12-19 23:39:00 +0100

    feat(patient): Création de l'entité patient

---
+++ app/DoctrineMigrations/Version20161219223811.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161219223811 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE patient (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(50) NOT NULL, prenom VARCHAR(50) NOT NULL, date_de_naissance DATE NOT NULL, telephone1 VARCHAR(10) NOT NULL, telephone2 VARCHAR(10) NOT NULL, telephone3 VARCHAR(10) NOT NULL, activite VARCHAR(100) NOT NULL, notes LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE patient');
+    }
+}
---
+++ src/AppBundle/Entity/Patient.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Patient
+ *
+ * @ORM\Entity
+ * @ORM\Table
+ */
+class Patient
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=50)
+     * @Assert\NotBlank()
+     * @Assert\Length(max = 50)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=50)
+     * @Assert\NotBlank()
+     * @Assert\Length(max = 50)
+     */
+    private $prenom;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="date")
+     */
+    private $dateDeNaissance;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=10)
+     * @Assert\Length(max = 10)
+     */
+    private $telephone1;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=10)
+     * @Assert\Length(max = 10)
+     */
+    private $telephone2;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=10)
+     * @Assert\Length(max = 10)
+     */
+    private $telephone3;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=100)
+     * @Assert\Length(max = 100)
+     */
+    private $activite;
+
+    /**
+     * @var string
+     * @ORM\Column(type="text")
+     */
+    private $notes;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     * @return Patient
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string $prenom
+     * @return Patient
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateDeNaissance()
+    {
+        return $this->dateDeNaissance;
+    }
+
+    /**
+     * @param \DateTime $dateDeNaissance
+     * @return Patient
+     */
+    public function setDateDeNaissance($dateDeNaissance)
+    {
+        $this->dateDeNaissance = $dateDeNaissance;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTelephone1()
+    {
+        return $this->telephone1;
+    }
+
+    /**
+     * @param string $telephone1
+     * @return Patient
+     */
+    public function setTelephone1($telephone1)
+    {
+        $this->telephone1 = $telephone1;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTelephone2()
+    {
+        return $this->telephone2;
+    }
+
+    /**
+     * @param string $telephone2
+     * @return Patient
+     */
+    public function setTelephone2($telephone2)
+    {
+        $this->telephone2 = $telephone2;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTelephone3()
+    {
+        return $this->telephone3;
+    }
+
+    /**
+     * @param string $telephone3
+     * @return Patient
+     */
+    public function setTelephone3($telephone3)
+    {
+        $this->telephone3 = $telephone3;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getActivite()
+    {
+        return $this->activite;
+    }
+
+    /**
+     * @param string $activite
+     * @return Patient
+     */
+    public function setActivite($activite)
+    {
+        $this->activite = $activite;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNotes()
+    {
+        return $this->notes;
+    }
+
+    /**
+     * @param string $notes
+     * @return Patient
+     */
+    public function setNotes($notes)
+    {
+        $this->notes = $notes;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->prenom . ' ' . $this->nom;
+    }
+}
